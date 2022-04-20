b514336f84dc8efa171c7a89394f720c9c1223d8
Anton Savinykh <freevolnost@gmail.com>
2022-04-20 19:36:15 +0300

    first sprint4 commit

---
+++ src/main/java/com/ya/Account.java
@@ -0,0 +1,21 @@
+package com.ya;
+
+public class Account {
+
+    private final String name;
+
+    public Account(String name) {
+        this.name = name;
+    }
+
+    public boolean checkNameToEmboss() {
+        if (name.length() <= 3 || name.length() >= 19) {
+            return false;
+        } else if (name.length() - name.replace(" ", "").length() != 1) {
+            return false;
+        } else if (name.startsWith(" ") || name.endsWith(" ")) {
+            return false;
+        } else
+            return true;
+    }
+}
